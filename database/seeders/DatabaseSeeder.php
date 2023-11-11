a8d5a64136960082dd31e6e5f874f84e842a4072
Alihan Öztürk <alihanozturk364@gmail.com>
2023-11-11 16:09:17 +0300

    match section created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -53,6 +53,61 @@ class DatabaseSeeder extends Seeder
 
             error_log('Match Section Installed.');
 
+            Sections::create([
+                "page" => "index",
+                "section" => "about",
+                "title" => "WE ARE PROFESSIONAL TEAM ESPORT",
+                "sub_title" => "Who We Are",
+                "detail" => "Distinctively provide acces mutfuncto users whereas transparent proceses somes ncentivize eficient functionalities rather than an extensible archtectur services and cross",
+                "cover" => "https://www.google.com.tr/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fillustrations%2Fesports&psig=AOvVaw0gq9qdm3OsXfJsyamXMKaj&ust=1699794312106000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNDthLOBvIIDFQAAAAAdAAAAABAR",
+                "content" => '<div class="about-wrapper">
+                <div class="section-header">
+                    <p>who we are</p>
+                    <h2>we are professional team esport</h2>
+                </div>
+                <div class="about-content">
+                    <p>Distinctively provide acces mutfuncto users whereas transparent
+                        proceses somes ncentivize eficient functionalities rather than an
+                        extensible archtectur services and cross</p>
+                    <ul class="about-list">
+                        <li class="about-item d-flex flex-wrap">
+                            <div class="about-item-thumb">
+                                <img src="assets/images/about/icon-1.png" alt="Icon">
+                            </div>
+                            <div class="about-item-content">
+                                <h5>103k Community Earning</h5>
+                                <p>Distinctively provide acces mutfuncto users whereas
+                                    communicate leveraged services</p>
+                            </div>
+                        </li>
+                        <li class="about-item d-flex flex-wrap">
+                            <div class="about-item-thumb">
+                                <img src="assets/images/about/icon-2.png" alt="Icon">
+                            </div>
+                            <div class="about-item-content">
+                                <h5>34m+ Registered Players</h5>
+                                <p>Distinctively provide acces mutfuncto users whereas
+                                    communicate leveraged services</p>
+                            </div>
+                        </li>
+                        <li class="about-item d-flex flex-wrap">
+                            <div class="about-item-thumb">
+                                <img src="assets/images/about/icon-3.png" alt="Icon">
+                            </div>
+                            <div class="about-item-content">
+                                <h5>240k Streams Complete</h5>
+                                <p>Distinctively provide acces mutfuncto users whereas
+                                    communicate leveraged services</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>',
+                "status" => 1
+            ]);
+
+            error_log('About Section Installed.');
+
         
         
     }
