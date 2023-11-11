cb8f5dda5c2f11ebb79fcb43502550eff12daaf0
Alihan Öztürk <alihanozturk364@gmail.com>
2023-11-11 15:41:52 +0300

    header completed

--- resources/views/land/master.blade.php
+++ resources/views/land/master.blade.php
@@ -123,65 +123,18 @@
 							<div class="menu-area">
 								<ul class="menu">
 									<li>
-                                        <a href="#0" class="active">Home</a>
-                                        <ul class="submenu">
-                                            <li><a href="index.html">Home One</a></li>
-                                            <li><a href="index-2.html" class="active">Home Two</a></li>
-                                        </ul>
+                                        <a href="/" class="active">Home</a>
                                     </li>
 
 									<li>
-										<a href="#0">Features</a>
-										<ul class="submenu">
-											<li><a href="about.html">About</a></li>
-											<li><a href="gallery.html">gallery</a></li>
-											<li>
-												<a href="#0">games</a>
-												<ul class="submenu">
-													<li><a href="game-list.html">game list 1</a></li>
-													<li><a href="game-list2.html">game list 2</a></li>
-												</ul>
-											</li>
-											<li><a href="partners.html">partners</a></li>
-											<li>
-												<a href="#0">teams</a>
-												<ul class="submenu">
-													<li><a href="team.html">team</a></li>
-													<li><a href="team-single.html">team single</a></li>
-												</ul>
-											</li>
-											<li>
-												<a href="#0">accounts</a>
-												<ul class="submenu">
-													<li><a href="login.html">login</a></li>
-													<li><a href="signup.html">signup</a></li>
-												</ul>
-											</li>
-											<li>
-												<a href="#0">Shop</a>
-												<ul class="submenu">
-													<li><a href="shop.html">shop</a></li>
-													<li><a href="shop-single.html">Shop Details</a></li>
-													<li><a href="cart-page.html">Cart Page</a></li>
-												</ul>
-											</li>
-											<li><a href="404.html">404 Page</a></li>
-
-										</ul>
+										<a href="/teams">Teams</a>
 									</li>
-									<li><a href="achievements.html">achievement</a></li>
-									<li>
-										<a href="#0">Blog</a>
-										<ul class="submenu">
-											<li><a href="blog.html">Blog</a></li>
-											<li><a href="blog-2.html">Blog 2</a></li>
-											<li><a href="blog-single.html">Blog Single</a></li>
-										</ul>
-									</li>
-									<li><a href="contact.html">Contact</a></li>
+									<li><a href="/tournaments">Tournaments</a></li>
+									<li><a href="/fixture">Fixture</a></li>
+									<li><a href="/support">Support</a></li>
 								</ul>
-								<a href="login.html" class="login"><i class="icofont-user"></i> <span>LOG IN</span> </a>
-								<a href="signup.html" class="signup"><i class="icofont-users"></i> <span>SIGN UP</span></a>
+								<a href="/app/" class="login"><i class="icofont-user"></i> <span>LOG IN</span> </a>
+								
 
 								<!-- toggle icons -->
 								<div class="header-bar d-lg-none">
