509912eb57e1878d5978d2901f9049302b0f6850
Alihan Öztürk <alihanozturk364@gmail.com>
2023-12-24 12:52:54 +0300

    tournament status added

--- resources/views/app/tournaments.blade.php
+++ resources/views/app/tournaments.blade.php
@@ -5,7 +5,11 @@
         @foreach ($tournaments as $t)
         <div class="col-4">
             <div class="card shadow-sm">
-                <img src="{{$t->cover}}" height="225" alt="" style="background-size:cover">
+              
+                <img src="{{$t->cover}}" height="225" alt="" >
+                <div class="alert alert-{{$t->status_info['color']}}" role="alert" style="background-size:cover;position:absolute;top:0;right:0;margin:10px;padding:4px;">
+                  {{$t->status_info['title']}}
+                </div>
                 <div class="card-body">
                     <h4 class="card-title">{{$t->title}}</h4>
                   <p class="card-text">{{$t->description}}</p>
